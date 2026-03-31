ee19ffbc14d134e7b8f0d8852deb4528b73feb2b
Papec21 <idunno2137@gmail.com>
2026-03-31 18:17:08 +0200

    Added grid to camera

--- camera_tutorial.py
+++ camera_tutorial.py
@@ -21,7 +21,16 @@ while True:
     # height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     # print(f"Current Resolution: {width}x{height}")
 
-    cv2.rectangle(frame, (180, 100), (460, 380), (255, 255, 255), 3)
+    cv2.rectangle(frame, (185, 105), (455, 375), (255, 255, 255), 1)
+    for row in range(3):
+        for col in range(3):
+            x1 = col * 90 + 185
+            y1 = row * 90 + 105
+            x2 = (col + 1) * 90 + 185
+            y2 = (row + 1) * 90 + 105
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 255), 1)
+
+
     cv2.imshow("Robot Eye - press 'q' to quit", frame)
 
     # Stop when 'q' is pressed
