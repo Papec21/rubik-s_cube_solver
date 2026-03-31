83c912dd65ffc755e733796559870c2c0b8e00a7
Papec21 <idunno2137@gmail.com>
2026-03-31 18:05:08 +0200

    Added rectangle over the camera

--- camera_tutorial.py
+++ camera_tutorial.py
@@ -17,6 +17,11 @@ while True:
         print("Error: Could not receive frame.")
         break
 
+    # width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    # height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    # print(f"Current Resolution: {width}x{height}")
+
+    cv2.rectangle(frame, (180, 100), (460, 380), (255, 255, 255), 3)
     cv2.imshow("Robot Eye - press 'q' to quit", frame)
 
     # Stop when 'q' is pressed
